31b49e6f197c829883464570e1389045d4de85f3
Maayan Glikser <maayan@500tech.com>
2021-12-27 15:06:41 +0200

    remove processes that run in jamf

--- install.sh
+++ install.sh
@@ -4,49 +4,12 @@ echo "Setting up your Mac..."
 
 cd ~/.dotfiles || { echo "You must clone dotfiles into ~/.dotfiles"; exit 1; }
 
-# Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-fi
-
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Update Homebrew recipes
-brew update
-
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
-
 # Create a CODE directory
 mkdir $HOME/Code
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
-# reload zshrc to apply to the current session
-source $HOME/.zshrc
-
-# install LTS version of node
-n lts
-
-# only install yarn after node has been installed to satisfy the dependency
-brew install yarn
-
-# Install scm breeze
-git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
-~/.scm_breeze/install.sh
-
-# Fix zsh permissions
-sudo chmod -R 755 /usr/local/share/zsh
-sudo chown -R root:staff /usr/local/share/zsh
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source .macos
